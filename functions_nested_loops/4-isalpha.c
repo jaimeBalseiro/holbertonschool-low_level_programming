d1528a57dc3c133075e0e6c924e62d1d7ff0edc4
jaimeBalseiro <jaime.diaz0303@gmail.com>
2022-06-09 19:51:29 +0000

    checks for alphanumeric character

--- functions_nested_loops/4-isalpha.c
+++ functions_nested_loops/4-isalpha.c
@@ -3,12 +3,20 @@
 /**
  * _isalpha - function that checks for alphabetic character
  * @c: The imput alphabetic character to check
- * Return: int.
+ * Return: 1 if c is a letter, lower or upper case
  */
 int _isalpha(int c)
 {
-	if (c >= 60 && c <= 90) || (c >= 97 && c <= 122)
+	if (c >= 65 && c <= 90)
+	{
 		return (1);
+	}
+	else if (c >= 97 && c <= 122)
+	{
+		return (1);
+	}
 	else
+	{
 		return (0);
+	}
 }
