d0a0f0b49961a03af7dd736b1ac270fa5a718162
jaimeBalseiro <jaime.diaz0303@gmail.com>
2022-06-09 16:48:54 +0000

    2-main.h

---
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - prints 10 times the alphabet, in lowercase
+ * You can only use _putchar twice
+ * Return - void
+ */
+
+void print_alphabet_x10(void)
+{
+	char alphabet[26] = "abcdefghijklmnopqrstuvwxyz";
+	int counter;
+	int x;
+
+	while (x < 10)
+	{
+		for (counter = 0; counter < 26; x++)
+
+		{
+			_putchar(alphabet[counter]);
+		}
+		_putchar('\n');
+		x++;
+	}
+}
