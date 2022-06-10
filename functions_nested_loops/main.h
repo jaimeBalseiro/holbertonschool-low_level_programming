a8b7b32ee50447ce61d7cae13c0ce1314b5d6a59
jaimeBalseiro <jaime.diaz0303@gmail.com>
2022-06-10 00:49:29 +0000

    main.h

--- functions_nested_loops/main.h
+++ functions_nested_loops/main.h
@@ -1,6 +1,10 @@
+#ifndef MAIN
+#define MAIN
 int _putchar(char c);
 void print_alphabet(void);
 void print_alphabet_x10(void);
 int _islower(int c);
 int _isalpha(int c);
 int print_sign(int n);
+int _abs(int);
+int print_last_digit(int);
