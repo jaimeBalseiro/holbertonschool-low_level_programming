c37fb85200d233f8b0b75f4151df9aee627970d4
jaimeBalseiro <jaime.diaz0303@gmail.com>
2022-06-24 14:45:23 +0000

    program that prints all args it receives

---
+++ argc_argv/2-args.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - program that prints all arguments
+ * @argc: the # of args
+ * @argv: the names of args
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+	int i = 0;
+
+	while (argc--)
+	{
+		printf("%s\n", argv[i]);
+		i++;
+	}
+	return (0);
+}
