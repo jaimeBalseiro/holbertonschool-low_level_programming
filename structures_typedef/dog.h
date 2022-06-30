45b6e6bd0149abe629d278c138e396100508c912
jaimeBalseiro <jaime.diaz0303@gmail.com>
2022-06-30 17:05:07 +0000

    header file dog

---
+++ structures_typedef/dog.h
@@ -0,0 +1,16 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - woof
+ * @name: name of dog
+ * @age: age of dog
+ * @owner: owner of dog
+ * Description: keeping track of dogs
+ */
+typedef struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+}
