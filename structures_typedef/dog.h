943df16815f49d0ca9d8e7be845eaa2aba58da20
jaimeBalseiro <jaime.diaz0303@gmail.com>
2022-07-01 04:23:34 +0000

    header file dog

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -14,6 +14,9 @@ typedef struct dog
 	float age;
 	char *owner;
 } dog_t;
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
 void print_dog(struct dog *d);
+void init_dog(struct dog *d, char *name, float age, char *owner);
 
 #endif /* DOG_H */
