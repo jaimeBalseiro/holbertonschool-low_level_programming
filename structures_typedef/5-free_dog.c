237892777e0eb173ada17086688c9e6966a755dc
jaimeBalseiro <jaime.diaz0303@gmail.com>
2022-07-01 17:35:29 +0000

    function that frees dogs

---
+++ structures_typedef/5-free_dog.c
@@ -0,0 +1,17 @@
+#include "dog.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * free_dog - freeing the dog
+ * @d: dog
+ * Return: void
+ */
+void free_dog(dog_t *d)
+{
+	if (d == NULL)
+		return;
+	free(d->name);
+	free(d->owner);
+	free(d);
+}
