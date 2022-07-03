c6961fd1deb012fc3e91751f07a9a08bdff2a4f7
jaimeBalseiro <jaime.diaz0303@gmail.com>
2022-07-03 01:24:18 +0000

    contains 5 functions

---
+++ function_pointers/3-op_functions.c
@@ -0,0 +1,51 @@
+#include "3-calc.h"
+/**
+ * op_add - adds 
+ * @a: first int
+ * @b: the other int
+ * Return: int
+ */
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+/**
+ * op_sub - substracts
+ * @a: first int
+ * @b: other int
+ * Return: int
+ */
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+/**
+ * op_mul - multplies
+ * @a: first int
+ * @b: other int
+ * Return: int
+ */
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+/**
+ * op_div - divides
+ * @a: first int
+ * @b: other int
+ * Return: int
+ */
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+/**
+ * op_mod - modulo
+ * @a: first int
+ * @b: other int
+ * Return: int
+ */
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
