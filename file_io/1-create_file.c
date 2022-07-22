e43535b34f5adb71f03c7e80069dc2c6691160f5
jaimeBalseiro <jaime.diaz0303@gmail.com>
2022-07-22 22:03:19 +0000

    creates a file

---
+++ file_io/1-create_file.c
@@ -0,0 +1,37 @@
+#include "main.h"
+
+/**
+ * create_file - creates a file
+ * @filename: filename
+ * @text_content: content writed in the file
+ * Return: 1 if success, -1 if fails
+ */
+int create_file(const char *filename, char *text_content)
+{
+	int fd;
+	int numletters;
+	int WRITE;
+
+	if (!filename)
+		return (-1);
+
+	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0000);
+
+	if (fd == -1)
+		return (-1);
+
+	if (!text_content)
+		text_content = "";
+
+	for (numletters = 0; text_content[numletters]; numletters++)
+		;
+
+	WRITE = write(fd, text_content, numletters);
+
+	if (WRITE == -1)
+		return (-1);
+
+	close(fd);
+
+	return (1);
+}
